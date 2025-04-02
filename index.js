a504e33f2a25ddd6f74f59117594c5a3f5e50df5
Denta Muhajir <dentamuhajir@gmail.com>
2025-04-02 16:54:23 +0700

    Add mongo & mongoose and create a database connection

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 app.listen(3000, () => {
@@ -7,4 +8,13 @@ app.listen(3000, () => {
 
 app.get('/', (req, res) => {
     res.send("Hello from Node API with nodemon")
+})
+
+
+mongoose.connect("mongodb+srv://dentamuhajir:7gYr72jCQ1dsxsA3@backenddb.oiomyfk.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB")
+.then(() => {
+    console.log("Connected to database!")
+})
+.catch((error)=> {
+    console.log("Connection is failed! " + error.message)
 })
