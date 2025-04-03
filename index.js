c8129897f5b866925ee778a2dc0702b8cb829d6a
Denta Muhajir <dentamuhajir@gmail.com>
2025-04-03 17:33:01 +0700

    Fixing error product.create is not a function

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ const mongoose = require('mongoose')
 const Product = require('./models/product.model.js');
 
 const app = express()
- 
-app.use(express.json())
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+ app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send("Hello from Node API with nodemon")
@@ -26,7 +21,10 @@ app.post('/api/products', async (req, res) => {
 
 mongoose.connect("mongodb+srv://dentamuhajir:eW95wn1yPBfD6oPS@backenddb.oiomyfk.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB&ssl=true")
 .then(() => {
-    console.log("Connected to database!")
+    console.log("Connected to database! ")
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000 ')
+    })
 })
 .catch((error)=> {
     console.log("Connection is failed! " + error.message)
