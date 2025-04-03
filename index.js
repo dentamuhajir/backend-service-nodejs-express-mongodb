fd83ea2817e306113c202d83daec71cb814858b2
Denta Muhajir <dentamuhajir@gmail.com>
2025-04-03 17:36:24 +0700

    Get data product

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.get('/', (req, res) => {
     res.send("Hello from Node API with nodemon")
 })
 
+app.get('/api/products', async (req, res) => {
+    try {
+        const product = await Product.find({})
+        res.status(200).json(product) 
+    } catch(error) {
+        res.status(500).json({ message: error.message})
+    }
+})
+
 app.post('/api/products', async (req, res) => {
     try {
         const product = await Product.create(req.body)
