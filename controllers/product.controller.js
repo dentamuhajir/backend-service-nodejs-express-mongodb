74c684fb289a513a6e36b6932d28f9f6d3c81516
Denta Muhajir <dentamuhajir@gmail.com>
2025-04-08 16:11:05 +0700

    Update with post

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose')
 const Product = require('../models/product.model.js');
 
-const getProduct = (req, res, next ) => {
+const getProduct = async(request, response, next) => {
+    try { 
+        const product = await Product.find({})
+        response.status(200).json(product)  
+    } catch(error) {
+        //request.status(500).json({ message: error.message})
+        next(error)
+    }
+}
+
+const postProduct = async(request, response, next) => {
     try {
-        res.send("Hello from Node API with nodemon")   
-    } catch(e) {
-        next(e)
+        const product = await Product.create(request.body)
+        response.status(200).json(product)
+    } catch(error) {
+        next(error)
     }
 }
 
 module.exports = {
-    getProduct
+    getProduct,
+    postProduct
 }
--- routes/api.route.js
+++ routes/api.route.js
@@ -3,5 +3,6 @@ const productController = require('../controllers/product.controller.js')
 const productRouter = new express.Router();
 
 productRouter.get('/api/products', productController.getProduct)
+productRouter.post('/api/products', productController.postProduct)
 
 module.exports = productRouter 
