6f0e13c6bef8e01b3ceac1a0bbdff08b079a5fe2
Denta Muhajir <dentamuhajir@gmail.com>
2025-04-10 20:11:58 +0700

    Create service products

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 const Product = require('../models/product.model.js');
+const service = require('../services/product.service.js')
 
 const getProduct = async(request, response, next) => {
     try { 
-        const product = await Product.find({})
+        const product = await service.getProducts() 
         response.status(200).json(product)  
     } catch(error) {
         next(error)
@@ -15,11 +16,10 @@ const postProduct = async(request, response, next) => {
         const product = await Product.create(request.body)
         response.status(200).json(product)
     } catch(error) {
-         next(error)
+        next(error)
     }
 }
-
 module.exports = {
     getProduct,
-    postProduct
+    postProduct,
 }
