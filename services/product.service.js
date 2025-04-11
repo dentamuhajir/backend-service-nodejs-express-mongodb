bc8c867896430209ebdb5600e9bb2ee06399b36b
Denta Muhajir <dentamuhajir@gmail.com>
2025-04-11 20:54:41 +0700

    Add service getDetailProduct

--- services/product.service.js
+++ services/product.service.js
@@ -1,8 +1,13 @@
 const Product = require('../models/product.model.js')
 const getProducts  = async() => {    
     return await Product.find({})
-} 
+}
+
+const getDetailProduct = async(id) => {
+    return await Product.findById(id)    
+}
 
 module.exports = {
-    getProducts
+    getProducts,
+    getDetailProduct
 }
