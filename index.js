c502c35ae9076024745501209512f3a9f1ba041c
Denta Muhajir <dentamuhajir@gmail.com>
2025-04-14 15:54:47 +0700

    Add winston logger and setup

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes/api.route.js')
+const winston = require('winston')
 const errorMiddleware  = require('./middleware/error.middleware.js')
+const { combine, timestamp, printf, colorize, align } = winston.format;
+
+const logger = winston.createLogger({
+    level: process.env.LOG_LEVEL || 'info',
+    format: combine(
+        colorize({ all: true }),
+        timestamp({
+        format: 'YYYY-MM-DD hh:mm:ss.SSS A',
+        }),
+        align(),
+        printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
+    ),
+    transports: [new winston.transports.Console()],
+})
 
 const app = express()
 app.use(express.json())
 app.use(routes)
 app.use(errorMiddleware)
 
-
 mongoose.connect(process.env.DATABASE_URL).then(() => {
-    console.log("Connected to database! ")
+    logger.info('Connected to database!')
     app.listen(3000, () => {
-        console.log('Server is running on port 3000 ')
+        logger.info('Server is running on port 3000 ')
     })
 }).catch((error)=> {
-    console.log("Connection is failed! " + error.message)
+    logger.error("Connection is failed! " + error.message)
 })
