5bc43a43caa78a6e4a2c3c32bb6638c62a9a8d7b
Denta Muhajir <dentamuhajir@gmail.com>
2025-04-16 18:58:45 +0700

    Store a request body in const and looping

--- routes/api.route.js
+++ routes/api.route.js
@@ -5,6 +5,7 @@ const productRouter = new express.Router();
 productRouter.get('/api/products', productController.getProduct)
 productRouter.post('/api/products', productController.postProduct)
 productRouter.delete('/api/products/:productId', productController.deleteProduct)
+productRouter.delete('/api/products/multiple/delete', productController.deleteManyProduct)
 productRouter.get('/api/products/:productId', productController.getDetailProduct)
 productRouter.put('/api/products/update/:productId', productController.updateProduct)
 
